71852475242e9c796340720f7f06fe4d1bb7bb9d
Jason M <json.mooreb@gmail.com>
2025-08-10 12:19:26 -0400

    Renamed project to personal_inventory_app

--- inventory_console_app.py
+++ inventory_console_app.py
@@ -6,32 +6,53 @@ import time
 
 inventory = {}
 
+def main_menu():
+    #stuff
+    return
+
 # CRUD functions
 def add_inventory():
+
+    last_entry = None
+    print('[ ADDING Inventory . . . ]\n')
+
     while True:
-        new_inventory = input('To add inventory, enter item and quantity as formatted below. (Enter x to stop adding inventory)\n'
-                            '[item name], [quantity]: ')
-        last_entry = (item.strip(), int(quantity.strip()))
+        new_inventory = input('To add inventory, enter item and quantity as formatted below.\n'
+                              '(Enter x to stop adding inventory)\n'
+                              '(Enter r to remove the last recent entry)\n\n'
+                              '[item name], [quantity]: ').lower()
+        
         if new_inventory == 'x':
             break
-
-        # add statement to remove last entry into dictionary - incomplete
-        if new_inventory =='r':
-            inventory.pop(last_entry, None)
-        # remove last entry -- end
+        if new_inventory == 'r':
+            if last_entry in inventory:
+                inventory.pop(last_entry)
+                print(f'Removed: [{last_entry}, Qty: {last_quantity}]\n')
+                time.sleep(1)
+                continue
+            elif last_entry not in inventory:
+                print('No recent entries found.\n')
+                time.sleep(1)
+                continue
 
         try:
             item, quantity = new_inventory.split(',')
-            inventory[item.strip()] = int(quantity.strip())
-            print(f'\nAdded: {item.strip()}, Quantity: {quantity.strip()}')
-            print(inventory)
+            quantity = int(quantity.strip())
+            inventory[item.strip()] = quantity
+            print(f'Added: [{item.strip()}, Qty: {quantity}]\n')
+            time.sleep(1)
+            last_entry = item
+            last_quantity = quantity
             continue
         except ValueError:
-            print('\nPlease enter a valid quantity.\n')
+            print('Please enter a valid quantity.\n')
+            time.sleep(1)
+            continue
     return inventory
 
 def read_inventory():
-    #stuff
+    print(inventory)
+    # add a way to search the inventory
     return #stuff
 
 def edit_inventory():
